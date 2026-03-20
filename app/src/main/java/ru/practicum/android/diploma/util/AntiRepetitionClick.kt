35f4f8788459f2acc686b91592fa7aa204bdc32c
Sergei <Nicoanik@hotmail.com>
2026-03-20 21:31:19 +0300

    Fix Cognitive Complexity

--- app/src/main/java/ru/practicum/android/diploma/util/AntiRepetitionClick.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/AntiRepetitionClick.kt
@@ -4,20 +4,27 @@ import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.input.pointer.pointerInput
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 fun Modifier.antiRepetitionClick(
-    coroutineScope: CoroutineScope,
     lockTimeMillis: Long = LOCK_TIME,
     onClick: () -> Unit
 ): Modifier = composed {
+    val scope = rememberCoroutineScope()
     var isLocked by remember { mutableStateOf(false) }
+
+    fun startDelay() {
+        scope.launch {
+            delay(lockTimeMillis)
+            isLocked = false
+        }
+    }
 
     this.then(
         Modifier.pointerInput(Unit) {
@@ -25,14 +32,11 @@ fun Modifier.antiRepetitionClick(
                 while (true) {
                     awaitFirstDown(requireUnconsumed = false)
 
-                    if (!isLocked) {
-                        onClick()
-                        isLocked = true
-                        coroutineScope.launch {
-                            delay(lockTimeMillis)
-                            isLocked = false
-                        }
-                    }
+                    if (isLocked) continue
+
+                    onClick()
+                    isLocked = true
+                    startDelay()
                 }
             }
         }
--- app/src/main/java/ru/practicum/android/diploma/util/DebounceClick.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/DebounceClick.kt
@@ -4,22 +4,29 @@ import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.input.pointer.pointerInput
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 fun Modifier.debounceClick(
-    coroutineScope: CoroutineScope,
     delayMillis: Long = DEBOUNCE_TIME,
     useLastParam: Boolean = true,
     onDebouncedClick: () -> Unit
 ): Modifier = composed {
+    val scope = rememberCoroutineScope()
     var debounceJob: Job? by remember { mutableStateOf(null) }
+
+    fun startDelay() {
+        debounceJob = scope.launch {
+            delay(delayMillis)
+            onDebouncedClick()
+        }
+    }
 
     this.then(
         Modifier.pointerInput(Unit) {
@@ -27,16 +34,10 @@ fun Modifier.debounceClick(
                 while (true) {
                     awaitFirstDown(requireUnconsumed = false)
 
-                    if (useLastParam) {
-                        debounceJob?.cancel()
-                    }
+                    if (useLastParam) debounceJob?.cancel()
 
-                    if (debounceJob?.isCompleted == true || useLastParam) {
-                        debounceJob = coroutineScope.launch {
-                            delay(delayMillis)
-                            onDebouncedClick()
-                        }
-                    }
+                    if (debounceJob?.isCompleted != true && !useLastParam) continue
+                    startDelay()
                 }
             }
         }
