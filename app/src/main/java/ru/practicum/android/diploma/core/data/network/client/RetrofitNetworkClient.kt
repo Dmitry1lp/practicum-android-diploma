1484f8b1910ebc69ab2a25e4ca8904f9c7136a90
Dmitry1lp <kallisto66@mail.ru>
2026-03-22 22:07:35 +0300

    #8 Общие Data-классы.

--- app/src/main/java/ru/practicum/android/diploma/core/data/network/client/RetrofitNetworkClient.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/data/network/client/RetrofitNetworkClient.kt
@@ -3,6 +3,7 @@ package ru.practicum.android.diploma.core.data.network.client
 import android.util.Log
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import retrofit2.HttpException
 import ru.practicum.android.diploma.core.data.network.api.VacancyApi
 import ru.practicum.android.diploma.core.data.network.dto.AreasRequest
 import ru.practicum.android.diploma.core.data.network.dto.AreasResponse
@@ -11,6 +12,7 @@ import ru.practicum.android.diploma.core.data.network.dto.IndustriesResponse
 import ru.practicum.android.diploma.core.data.network.dto.Response
 import ru.practicum.android.diploma.core.data.network.dto.VacancyDetailsRequest
 import ru.practicum.android.diploma.core.data.network.dto.VacancySearchRequest
+import java.io.IOException
 
 class RetrofitNetworkClient(
     private val api: VacancyApi
@@ -38,14 +40,18 @@ class RetrofitNetworkClient(
                     else -> Response().apply { resultCode = RESULT_BAD_REQUEST }
                 }
                 response.apply { resultCode = RESULT_OK }
-            } catch (e: Throwable) {
-                Log.e("RetrofitNetworkClient", "Network error", e)
+            } catch (e: IOException) { // Проблемы с сетью, таймауты
+                Log.e(TAG, "Network error", e)
                 Response().apply { resultCode = RESULT_SERVER_ERROR }
+            } catch (e: HttpException) { // Ошибки HTTP, например 404, 500
+                Log.e(TAG, "HTTP error", e)
+                Response().apply { resultCode = e.code() }
             }
         }
     }
 
     companion object {
+        private const val TAG = "RetrofitNetworkClient"
         private const val RESULT_OK = 200
         private const val RESULT_BAD_REQUEST = 400
         private const val RESULT_SERVER_ERROR = 500
