2d0640794b0ee1d18672d560432e2b873e9e9c3b
dekry <de.krylov@vk.com>
2026-03-25 19:03:35 +0300

    feat(rowListItem) темизация VacancyListItem и добавление плейсхолдера

--- app/src/main/java/ru/practicum/android/diploma/core/presentation/components/ImageRowListItem.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/presentation/components/ImageRowListItem.kt
@@ -19,6 +19,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -58,7 +59,9 @@ fun ImageListItem(
                 .clip(shape),
             model = model,
             contentDescription = imageContentDescription,
-            placeholder = painterResource(R.drawable.ic_launcher_foreground)
+            placeholder = painterResource(R.drawable.placeholder_32),
+            error = painterResource(R.drawable.placeholder_32),
+            contentScale = ContentScale.Crop
         )
 
         content()
--- app/src/main/java/ru/practicum/android/diploma/core/presentation/components/VacancyListItem.kt
+++ app/src/main/java/ru/practicum/android/diploma/core/presentation/components/VacancyListItem.kt
@@ -1,10 +1,14 @@
 package ru.practicum.android.diploma.core.presentation.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import okhttp3.internal.immutableListOf
 import ru.practicum.android.diploma.app.ui.theme.AppTypography
 import ru.practicum.android.diploma.app.ui.theme.DiplomaTheme
 
@@ -16,23 +20,26 @@ fun VacancyListItem(
     modifier: Modifier = Modifier,
     model: Any? = null
 ) {
+    val textItems = remember {
+        immutableListOf(
+            title to AppTypography.titleMedium,
+            industry to AppTypography.bodyLarge,
+            salary to AppTypography.bodyLarge
+        )
+    }
+
     ImageListItem(
-        modifier = modifier,
+        modifier = modifier.background(MaterialTheme.colorScheme.background),
         model = model
     ) {
         Column {
-            Text(
-                text = title,
-                style = AppTypography.titleMedium
-            )
-            Text(
-                text = industry,
-                style = AppTypography.bodyLarge
-            )
-            Text(
-                text = salary,
-                style = AppTypography.bodyLarge
-            )
+            textItems.forEach { (text, style) ->
+                Text(
+                    text = text,
+                    style = style,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+            }
         }
     }
 }
@@ -40,7 +47,7 @@ fun VacancyListItem(
 @Preview
 @Composable
 private fun VacancyListItemPreviewLight() {
-    DiplomaTheme(false) {
+    DiplomaTheme() {
         VacancyListItem(
             model = null,
             title = "Android-разработчик, Москва",
