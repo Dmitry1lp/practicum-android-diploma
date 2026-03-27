84791e85b1d179a654991ea53297206ff33c3081
Sviridov-Ivan <sviridov_ivan@mail.ru>
2026-03-27 13:56:08 +0500

    feature-vacancy: interactor

---
+++ app/src/main/java/ru/practicum/android/diploma/feature/vacancy/domain/VacancyInteractor.kt
@@ -0,0 +1,24 @@
+package ru.practicum.android.diploma.feature.vacancy.domain
+
+import ru.practicum.android.diploma.core.domain.model.Vacancy
+
+class VacancyInteractor(
+    private val repository: VacancyRepository
+) {
+    // загрузка вакансии
+    suspend fun loadVacancy(id: String): VacancyResult {
+        return repository.getVacancy(id)
+    }
+    // избиратель для Избранного
+    suspend fun toggleFavorite(vacancy: Vacancy, isFavorite: Boolean) {
+        if (isFavorite) {
+            repository.removeFromFavorites(vacancy.id)
+        } else {
+            repository.addToFavourites(vacancy)
+        }
+    }
+    // статус вакансии (избрана или нет)
+    suspend fun isFavorite(id: String): Boolean {
+        return repository.isFavorite(id)
+    }
+}
