8d9bd790a372fd00a1cb9b7e9d282d1f6fc9362d
Sviridov-Ivan <sviridov_ivan@mail.ru>
2026-03-27 18:34:36 +0500

    feature-vacancy: VacancyViewModel + UIEvent (Share, Email, Phone)

--- app/src/main/java/ru/practicum/android/diploma/feature/vacancy/domain/VacancyInteractor.kt
+++ app/src/main/java/ru/practicum/android/diploma/feature/vacancy/domain/VacancyInteractor.kt
@@ -9,6 +9,7 @@ class VacancyInteractor(
     suspend fun loadVacancy(id: String): VacancyResult {
         return repository.getVacancy(id)
     }
+
     // избиратель для Избранного
     suspend fun toggleFavorite(vacancy: Vacancy, isFavorite: Boolean) {
         if (isFavorite) {
@@ -17,6 +18,7 @@ class VacancyInteractor(
             repository.addToFavourites(vacancy)
         }
     }
+
     // статус вакансии (избрана или нет)
     suspend fun isFavorite(id: String): Boolean {
         return repository.isFavorite(id)
