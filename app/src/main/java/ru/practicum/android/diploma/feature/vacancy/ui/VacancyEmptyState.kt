87d821b009d84ae9dffda5724fb4dcc8b74405e4
Sviridov-Ivan <sviridov_ivan@mail.ru>
2026-03-29 15:09:50 +0500

    feature-vacancy: UI. VacancyEmptyState + pic

---
+++ app/src/main/java/ru/practicum/android/diploma/feature/vacancy/ui/VacancyEmptyState.kt
@@ -0,0 +1,24 @@
+package ru.practicum.android.diploma.feature.vacancy.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.core.presentation.components.StateInfo
+
+@Composable
+fun VacancyEmptyState(
+) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        StateInfo(
+            image = R.drawable.img_empty_vacancy,
+            text = stringResource(R.string.error_vacancy_not_found)
+        )
+    }
+}
