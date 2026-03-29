4a38dc10858b05df37d6de5fbbc91f2b4b17721d
Sergei <Nicoanik@hotmail.com>
2026-03-29 20:28:47 +0300

    Add SearchBar

--- app/src/main/java/ru/practicum/android/diploma/feature/filters/ui/IndustriesScreen.kt
+++ app/src/main/java/ru/practicum/android/diploma/feature/filters/ui/IndustriesScreen.kt
@@ -1,15 +1,11 @@
 package ru.practicum.android.diploma.feature.filters.ui
 
-import android.media.Image
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.content.MediaType.Companion.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.selection.selectable
@@ -24,12 +20,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kotlinx.coroutines.flow.MutableStateFlow
 import ru.practicum.android.diploma.R
@@ -48,6 +40,7 @@ fun IndustriesScreen(
     onBackClick: () -> Unit
 ) {
     val state by stateViewModel.collectAsStateWithLifecycle()
+    var text by remember { mutableStateOf("") }
 
     Scaffold(
         topBar = {
@@ -60,17 +53,21 @@ fun IndustriesScreen(
         Column(
             modifier = modifier.padding(paddingValues)
         ) {
+            SearchBar(
+                text = text,
+                onTextChange = { text = it }
+            )
             if (state.industries.isNotEmpty()) {
-                ShowIndustries(state.industries)
+                ShowContent(state.industries)
             } else {
-                ShowPlaceholder()
+                PlaceholderErrorListFetch()
             }
         }
     }
 }
 
 @Composable
-private fun ShowIndustries(industries: List<Industry>) {
+private fun ShowContent(industries: List<Industry>) {
     var selectedOption by remember { mutableStateOf<Industry?>(null) }
 
     LazyColumn(
@@ -82,7 +79,7 @@ private fun ShowIndustries(industries: List<Industry>) {
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(60.dp)
+                    .height(AppDimensions.LabelActionListItem.itemHeight)
                     .selectable(
                         selected = (industry == selectedOption),
                         onClick = { selectedOption = industry }
@@ -107,32 +104,6 @@ private fun ShowIndustries(industries: List<Industry>) {
                 )
             }
         }
-    }
-}
-
-@Composable
-private fun ShowPlaceholder() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(horizontal = AppDimensions.paddingMedium)
-                .padding(top = 122.dp),
-            painter = painterResource(R.drawable.error_list_fetch),
-            contentDescription = "error_list_fetch",
-            contentScale = ContentScale.Crop
-        )
-        Text(
-            modifier = Modifier
-                .width(268.dp)
-                .padding(top = AppDimensions.paddingMedium),
-            text = stringResource(R.string.error_list_fetch),
-            style = AppTypography.titleMedium,
-            textAlign = TextAlign.Center
-        )
     }
 }
 
---
+++ app/src/main/java/ru/practicum/android/diploma/feature/filters/ui/SalaryInputField.kt
@@ -0,0 +1,152 @@
+package ru.practicum.android.diploma.feature.filters.ui
+
+import android.graphics.Rect
+import android.view.ViewTreeObserver
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.State
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.app.ui.theme.AppDimensions
+import ru.practicum.android.diploma.app.ui.theme.AppTypography
+import ru.practicum.android.diploma.app.ui.theme.Black
+import ru.practicum.android.diploma.app.ui.theme.Blue
+
+@Composable
+fun SalaryInputField(
+    text: String,
+    onTextChange: (String) -> Unit
+) {
+    val focusManager = LocalFocusManager.current
+    val interactionSource = remember { MutableInteractionSource() }
+    val isFocused by interactionSource.collectIsFocusedAsState()
+    val isKeyboardVisible by rememberIsKeyboardVisible()
+
+    LaunchedEffect(isKeyboardVisible) {
+        if (!isKeyboardVisible && isFocused) {
+            focusManager.clearFocus()
+        }
+    }
+
+    Box(
+        modifier = Modifier
+            .height(AppDimensions.FiltersScreen.heightBoxInputField)
+            .padding(horizontal = AppDimensions.paddingMedium),
+        contentAlignment = Alignment.Center
+    ) {
+        BasicTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(AppDimensions.FiltersScreen.heightTextField)
+                .background(
+                    shape = RoundedCornerShape(AppDimensions.FiltersScreen.cornerRadius),
+                    color = MaterialTheme.colorScheme.surfaceVariant
+                ),
+            interactionSource = interactionSource,
+            value = text,
+            onValueChange = onTextChange,
+            textStyle = AppTypography.bodyLarge.copy(
+                color = MaterialTheme.colorScheme.onSecondary,
+                textAlign = TextAlign.Start
+            ),
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            cursorBrush = SolidColor(Blue),
+            decorationBox = { innerTextField ->
+                Row(
+                    modifier = Modifier.padding(AppDimensions.FiltersScreen.rowPaddingTextField),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Column(
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Text(
+                            text = stringResource(R.string.label_expected_salary),
+                            style = AppTypography.labelMedium,
+                            color = when {
+                                text.isEmpty() && !isFocused -> MaterialTheme.colorScheme.outline
+                                isFocused -> Blue
+                                else -> Black
+                            }
+                        )
+                        Box {
+                            innerTextField()
+                            if (text.isEmpty()) {
+                                Text(
+                                    stringResource(R.string.hint_salary_amount),
+                                    style = AppTypography.bodyLarge,
+                                    color = MaterialTheme.colorScheme.outline
+                                )
+                            }
+                        }
+                    }
+                    if (text.isNotEmpty() && isFocused) {
+                        Icon(
+                            modifier = Modifier.clickable(onClick = { onTextChange("") }),
+                            painter = painterResource(R.drawable.ic_close_24),
+                            contentDescription = null,
+                            tint = Black
+                        )
+                    }
+                }
+            }
+        )
+    }
+}
+
+@Composable
+private fun rememberIsKeyboardVisible(): State<Boolean> {
+    val view = LocalView.current
+    var keyboardHeight by remember { mutableIntStateOf(0) }
+
+    DisposableEffect(view) {
+        val listener = ViewTreeObserver.OnGlobalLayoutListener {
+            val rect = Rect()
+            view.getWindowVisibleDisplayFrame(rect)
+            val screenHeight = view.rootView.height
+            keyboardHeight = screenHeight - rect.bottom
+        }
+
+        view.viewTreeObserver.addOnGlobalLayoutListener(listener)
+
+        onDispose {
+            view.viewTreeObserver.removeOnGlobalLayoutListener(listener)
+        }
+    }
+
+    return remember {
+        derivedStateOf {
+            keyboardHeight > view.height * 0.15
+        }
+    }
+}
