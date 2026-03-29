0702463f87e2578883d20221172623b0435d73b8
Dmitry1lp <kallisto66@mail.ru>
2026-03-29 16:17:52 +0300

    #22 Поиск вакансий, Верстка экрана. Изменение StateInfo.kt -Сделал плейсхолдеры

--- app/src/main/java/ru/practicum/android/diploma/feature/search/ui/NotFoundPlaceholder.kt
+++ app/src/main/java/ru/practicum/android/diploma/feature/search/ui/NotFoundPlaceholder.kt
@@ -1,18 +1,12 @@
 package ru.practicum.android.diploma.feature.search.ui
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,35 +14,22 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.app.ui.theme.DiplomaTheme
+import ru.practicum.android.diploma.core.presentation.components.StateInfo
 
 @Composable
 fun NotFoundPlaceholder() {
-    Column(
+    Box(
         modifier = Modifier
             .fillMaxSize()
             .padding(24.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        contentAlignment = Alignment.Center
     ) {
-        Chip(text = stringResource(R.string.no_vacancies_found))
-
-        Spacer(modifier = Modifier.weight(1f))
-
-        Image(
-            painter = painterResource(id = R.drawable.ic_placeholder_not_found),
-            contentDescription = stringResource(R.string.error_vacancies_list)
-        )
-
-        Text(
+        StateInfo(
+            image = R.drawable.ic_placeholder_not_found,
             text = stringResource(R.string.error_vacancies_list),
-            modifier = Modifier.padding(top = 16.dp, bottom = 16.dp),
-            color = MaterialTheme.colorScheme.onBackground,
-            style = MaterialTheme.typography.titleMedium,
             textAlign = TextAlign.Center,
             lineHeight = 16.sp
         )
-
-        Spacer(modifier = Modifier.weight(1f))
     }
 }
 
--- app/src/main/java/ru/practicum/android/diploma/feature/search/ui/NotInternetPlaceholder.kt
+++ app/src/main/java/ru/practicum/android/diploma/feature/search/ui/NotInternetPlaceholder.kt
@@ -1,46 +1,34 @@
 package ru.practicum.android.diploma.feature.search.ui
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.app.ui.theme.DiplomaTheme
+import ru.practicum.android.diploma.core.presentation.components.StateInfo
 
 @Composable
 fun NoInternetPlaceholder() {
-    Column(
+    Box(
         modifier = Modifier
             .fillMaxSize()
             .padding(24.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        contentAlignment = Alignment.Center
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_placeholder_not_internet),
-            contentDescription = stringResource(R.string.error_no_internet)
-        )
-
-        Text(
-            text = stringResource(R.string.error_no_internet),
-            modifier = Modifier.padding(top = 16.dp, bottom = 16.dp),
-            color = MaterialTheme.colorScheme.onBackground,
-            style = MaterialTheme.typography.titleMedium
+        StateInfo(
+            image = R.drawable.ic_placeholder_not_internet,
+            text = stringResource(R.string.error_no_internet)
         )
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
