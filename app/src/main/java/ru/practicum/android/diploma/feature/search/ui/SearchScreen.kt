6ace884d41a16eab4057d758f4bc649b3449358c
dekry <de.krylov@vk.com>
2026-04-02 01:29:36 +0300

    build: добавить поддержку неизменяемых коллекций для безопасного состояния UI
    **Зависимости**
    - Добавлена библиотека kotlinx-collections-immutable версии 0.4.0
    
    **Refactoring VacancyList**
    - Тип параметра vacancies изменён с `List<Vacancy>` на `ImmutableList<Vacancy>`
    - Использование неизменяемых коллекций гарантирует, что список не будет мутироваться между рекомпозициями
    - Это особенно важно для списков, передаваемых из ViewModel в Compose UI
    - Добавлена версия `collections = "0.4.0"` для kotlinx-collections-immutable

--- app/src/main/java/ru/practicum/android/diploma/feature/search/ui/SearchScreen.kt
+++ app/src/main/java/ru/practicum/android/diploma/feature/search/ui/SearchScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import kotlinx.collections.immutable.toImmutableList
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.core.domain.model.Vacancy
 import ru.practicum.android.diploma.core.presentation.components.StateInfo
@@ -52,7 +53,7 @@ fun SearchScreen(
                     VacancyState.Loading -> CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
 
                     is VacancyState.Content -> VacancyList(
-                        vacancies = stateContent.vacancies,
+                        vacancies = stateContent.vacancies.toImmutableList(),
                         onVacancyClick = onVacancyClick,
                         onLoadNextPage = onLoadNextPage,
                         isLoadingNextPage = state.isNextPageLoading,
