fd038bd66f6585f21c755ad67f5c760c9331c12e
Dmitry1lp <kallisto66@mail.ru>
2026-04-02 18:57:38 +0300

    #72,83,85 Баг -пофиксил баги с плейсхолдерами -пофиксил баги с пагинацией

--- app/src/main/java/ru/practicum/android/diploma/feature/search/presentation/SearchViewModel.kt
+++ app/src/main/java/ru/practicum/android/diploma/feature/search/presentation/SearchViewModel.kt
@@ -1,5 +1,6 @@
 package ru.practicum.android.diploma.feature.search.presentation
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Job
@@ -40,6 +41,7 @@ class SearchViewModel(
         maxPages = 1
 
         val query = VacancyQuery(text = queryText, page = currentPage)
+        Log.d("PAGINATION0", "Requesting page = $currentPage")
         when (val result = vacancyRepository.searchVacancies(query)) {
             is Resource.Success -> {
                 val (vacancies, totalPages, found) = result.data
@@ -102,11 +104,12 @@ class SearchViewModel(
                 it.copy(isNextPageLoading = true)
             }
 
+            currentPage++
             val query = VacancyQuery(
                 text = queryText,
                 page = currentPage
             )
-
+            Log.d("PAGINATION", "Requesting page = $currentPage")
             when (val result = vacancyRepository.searchVacancies(query)) {
                 is Resource.Success -> {
                     val (vacancies, totalPages) = result.data
@@ -119,7 +122,6 @@ class SearchViewModel(
                         )
                     }
 
-                    currentPage++
                     maxPages = totalPages
                 }
 
