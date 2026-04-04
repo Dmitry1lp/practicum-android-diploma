76d0acc7c520e4939df3cc6e264bd4bc4a7f28ea
Sviridov-Ivan <sviridov_ivan@mail.ru>
2026-04-04 17:55:19 +0500

    feature-vacancy: clickable contacts

--- app/src/main/java/ru/practicum/android/diploma/feature/vacancy/ui/components/VacancyContacts.kt
+++ app/src/main/java/ru/practicum/android/diploma/feature/vacancy/ui/components/VacancyContacts.kt
@@ -1,9 +1,15 @@
 package ru.practicum.android.diploma.feature.vacancy.ui.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.app.ui.theme.AppDimensions
@@ -17,23 +23,52 @@ fun VacancyContacts(
     onPhoneClick: (String) -> Unit,
     onEmailClick: (String) -> Unit
 ) {
-    Text(
+    Column(
         modifier = Modifier.padding(top = AppDimensions.paddingBig),
-        text = stringResource(R.string.vacancy_contacts),
-        style = AppTypography.titleMedium
-    )
-
-    Text(text = contacts.name)
-
-    Text(
-        text = contacts.email,
-        modifier = Modifier.antiRepetitionClick { onEmailClick(contacts.email) }
-    )
-
-    contacts.phoneNumbers.forEach { phone ->
+        verticalArrangement = Arrangement.spacedBy(AppDimensions.paddingSmall)
+    ) {
         Text(
-            text = phone,
-            modifier = Modifier.antiRepetitionClick { onPhoneClick(phone) }
+            text = stringResource(R.string.vacancy_contacts),
+            style = AppTypography.titleMedium
         )
+
+        Text(
+            text = contacts.name,
+            style = AppTypography.bodyMedium
+        )
+
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(AppDimensions.paddingSmall)
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.ic_outline_attach_email_24),
+                contentDescription = null
+            )
+
+            Text(
+                text = contacts.email,
+                style = AppTypography.bodyMedium.copy(
+                    color = MaterialTheme.colorScheme.primary
+                ),
+                modifier = Modifier.antiRepetitionClick { onEmailClick(contacts.email) }
+            )
+        }
+        contacts.phoneNumbers.forEach { phone ->
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(AppDimensions.paddingSmall)
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.ic_outline_call_24),
+                    contentDescription = null
+                )
+                Text(
+                    text = phone,
+                    style = AppTypography.bodyMedium.copy(
+                        color = MaterialTheme.colorScheme.primary
+                    ),
+                    modifier = Modifier.antiRepetitionClick { onPhoneClick(phone) }
+                )
+            }
+        }
     }
 }
