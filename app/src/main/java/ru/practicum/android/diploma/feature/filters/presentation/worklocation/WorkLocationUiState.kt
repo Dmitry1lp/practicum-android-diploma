d1d1839f302879a7b658a3dad3582897166205e3
Sergei <Nicoanik@hotmail.com>
2026-04-06 18:46:12 +0300

    Change PC

--- app/src/main/java/ru/practicum/android/diploma/feature/filters/presentation/worklocation/WorkLocationUiState.kt
+++ app/src/main/java/ru/practicum/android/diploma/feature/filters/presentation/worklocation/WorkLocationUiState.kt
@@ -1,14 +1,34 @@
 package ru.practicum.android.diploma.feature.filters.presentation.worklocation
 
 import ru.practicum.android.diploma.core.domain.model.GeoArea
+import ru.practicum.android.diploma.feature.filters.presentation.filters.Clear
+import ru.practicum.android.diploma.feature.filters.presentation.filters.FiltersUiState
 
 data class WorkLocationUiState(
     val country: GeoArea.Country? = null,
     val region: GeoArea.Region? = null
 ) {
-    val locationString: String? = when {
-        country != null && region != null -> "${country.name}, ${region.name}"
-        country != null -> country.name
-        else -> null
+    companion object {
+        fun fromFiltersState(filtersState: FiltersUiState): WorkLocationUiState {
+            return WorkLocationUiState(
+                country = filtersState.country,
+                region = filtersState.region
+            )
+        }
+
+        fun fromCurrentFiltersState(filtersState: FiltersUiState): WorkLocationUiState {
+            return WorkLocationUiState(
+                country = filtersState.currentCountry,
+                region = filtersState.currentRegion
+            )
+        }
     }
 }
+
+data class WorkLocationActions(
+    val onBackClick: () -> Unit,
+    val onCountryClick: () -> Unit,
+    val onRegionClick: () -> Unit,
+    val onClearClick: (Clear) -> Unit,
+    val onApplyClick: (WorkLocationUiState) -> Unit
+)
