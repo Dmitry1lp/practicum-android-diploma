dcdd71b3a62e767c4e08e0dff15cd5a757773b56
Sergei <Nicoanik@hotmail.com>
2026-04-06 10:51:05 +0300

    Change PC

--- app/src/main/java/ru/practicum/android/diploma/feature/filters/presentation/worklocation/WorkLocationUiState.kt
+++ app/src/main/java/ru/practicum/android/diploma/feature/filters/presentation/worklocation/WorkLocationUiState.kt
@@ -1,14 +1,25 @@
 package ru.practicum.android.diploma.feature.filters.presentation.worklocation
 
 import ru.practicum.android.diploma.core.domain.model.GeoArea
+import ru.practicum.android.diploma.feature.filters.presentation.filters.FiltersUiState
 
 data class WorkLocationUiState(
     val country: GeoArea.Country? = null,
     val region: GeoArea.Region? = null
 ) {
-    val locationString: String? = when {
-        country != null && region != null -> "${country.name}, ${region.name}"
-        country != null -> country.name
-        else -> null
+    companion object {
+        fun fromFiltersState(filtersState: FiltersUiState): WorkLocationUiState {
+            return WorkLocationUiState(
+                country = filtersState.country,
+                region = filtersState.region
+            )
+        }
     }
 }
+
+data class WorkLocationActions(
+    val onBackClick: () -> Unit,
+    val onCountryClick: () -> Unit,
+    val onRegionClick: () -> Unit,
+    val onApplyClick: () -> Unit
+)
