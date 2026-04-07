65021840df5f8d4e34bc2008b53abc6e13144846
dekry <de.krylov@vk.com>
2026-04-07 16:19:44 +0300

    fix: Исправить WorkLocationActions и WorkLocationUiState

--- app/src/main/java/ru/practicum/android/diploma/feature/filters/presentation/worklocation/WorkLocationActions.kt
+++ app/src/main/java/ru/practicum/android/diploma/feature/filters/presentation/worklocation/WorkLocationActions.kt
@@ -1,8 +1,11 @@
 package ru.practicum.android.diploma.feature.filters.presentation.worklocation
 
+import ru.practicum.android.diploma.feature.filters.presentation.ClearTarget
+
 data class WorkLocationActions(
     val onBackClick: () -> Unit,
     val onCountryClick: () -> Unit,
     val onRegionClick: () -> Unit,
-    val onApplyClick: () -> Unit
+    val onClearClick: (ClearTarget) -> Unit,
+    val onApplyClick: (WorkLocationUiState) -> Unit
 )
