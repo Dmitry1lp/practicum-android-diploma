6b90d76df859163df22714c7856ceceea13a3afc
dekry <de.krylov@vk.com>
2026-04-07 16:19:44 +0300

    fix: Исправить WorkLocationActions и WorkLocationUiState

--- app/src/main/java/ru/practicum/android/diploma/feature/filters/presentation/worklocation/WorkLocationUiState.kt
+++ app/src/main/java/ru/practicum/android/diploma/feature/filters/presentation/worklocation/WorkLocationUiState.kt
@@ -1,13 +1,18 @@
 package ru.practicum.android.diploma.feature.filters.presentation.worklocation
 
 import ru.practicum.android.diploma.core.domain.model.GeoArea
-import ru.practicum.android.diploma.feature.filters.presentation.filters.Clear
 import ru.practicum.android.diploma.feature.filters.presentation.filters.FiltersUiState
 
 data class WorkLocationUiState(
     val country: GeoArea.Country? = null,
     val region: GeoArea.Region? = null
 ) {
+    val locationString: String? = when {
+        country != null && region != null -> "${country.name}, ${region.name}"
+        country != null -> country.name
+        else -> null
+    }
+
     companion object {
         fun fromFiltersState(filtersState: FiltersUiState): WorkLocationUiState {
             return WorkLocationUiState(
@@ -15,20 +20,5 @@ data class WorkLocationUiState(
                 region = filtersState.currentRegion
             )
         }
-    }
-}
-
-data class WorkLocationActions(
-    val onBackClick: () -> Unit,
-    val onCountryClick: () -> Unit,
-    val onRegionClick: () -> Unit,
-    val onClearClick: (Clear) -> Unit,
-    val onApplyClick: (WorkLocationUiState) -> Unit
-)
-) {
-    val locationString: String? = when {
-        country != null && region != null -> "${country.name}, ${region.name}"
-        country != null -> country.name
-        else -> null
     }
 }
