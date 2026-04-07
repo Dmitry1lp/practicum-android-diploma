d2d0816e7df8c3b6a116e699538c0cbacfe8e8e3
dekry <de.krylov@vk.com>
2026-04-07 09:56:56 +0300

    fix: Получать актуальные данные для стейтов экранов

--- app/src/main/java/ru/practicum/android/diploma/feature/filters/presentation/worklocation/WorkLocationActions.kt
+++ app/src/main/java/ru/practicum/android/diploma/feature/filters/presentation/worklocation/WorkLocationActions.kt
@@ -1,11 +1,8 @@
 package ru.practicum.android.diploma.feature.filters.presentation.worklocation
 
-import ru.practicum.android.diploma.feature.filters.presentation.ClearTarget
-
 data class WorkLocationActions(
     val onBackClick: () -> Unit,
     val onCountryClick: () -> Unit,
     val onRegionClick: () -> Unit,
-    val onClearClick: (ClearTarget) -> Unit,
-    val onApplyClick: (WorkLocationUiState) -> Unit
+    val onApplyClick: () -> Unit
 )
--- app/src/main/java/ru/practicum/android/diploma/feature/filters/presentation/worklocation/WorkLocationUiState.kt
+++ app/src/main/java/ru/practicum/android/diploma/feature/filters/presentation/worklocation/WorkLocationUiState.kt
@@ -1,27 +1,14 @@
 package ru.practicum.android.diploma.feature.filters.presentation.worklocation
 
 import ru.practicum.android.diploma.core.domain.model.GeoArea
-import ru.practicum.android.diploma.feature.filters.presentation.filters.Clear
-import ru.practicum.android.diploma.feature.filters.presentation.filters.FiltersUiState
 
 data class WorkLocationUiState(
     val country: GeoArea.Country? = null,
     val region: GeoArea.Region? = null
 ) {
-    companion object {
-        fun fromFiltersState(filtersState: FiltersUiState): WorkLocationUiState {
-            return WorkLocationUiState(
-                country = filtersState.currentCountry,
-                region = filtersState.currentRegion
-            )
-        }
+    val locationString: String? = when {
+        country != null && region != null -> "${country.name}, ${region.name}"
+        country != null -> country.name
+        else -> null
     }
 }
-
-data class WorkLocationActions(
-    val onBackClick: () -> Unit,
-    val onCountryClick: () -> Unit,
-    val onRegionClick: () -> Unit,
-    val onClearClick: (Clear) -> Unit,
-    val onApplyClick: (WorkLocationUiState) -> Unit
-)
